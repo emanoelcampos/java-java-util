4a6e0c47f4198107b347e1461f0d834f16f65c80
emanoelcampos <emanoel.cv@gmail.com>
2023-07-05 08:53:03 -0300

    array de string usando args

---
+++ src/module2/storing/reference/ArrayString.java
@@ -0,0 +1,15 @@
+package module2.storing.reference;
+
+public class ArrayString  {
+
+    public static void main(String[] args) {
+
+        //executar essa classe usando terminal
+        System.out.println("Funcionou");
+
+        //adicionar parametros no momento de executar a classe ArrayString
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
