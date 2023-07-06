f8c8a4cf478b51e22b07a3e668d93719aed34792
emanoelcampos <emanoel.cv@gmail.com>
2023-07-06 08:52:00 -0300

    uma forma diferente de manipular arrays

---
+++ src/module3/arrayslist/generics/GuardadorDeString.java
@@ -0,0 +1,25 @@
+package module3.arrayslist.generics;
+
+public class GuardadorDeString {
+
+    private String[] nomes;
+    private int posicaoLivre;
+
+    public GuardadorDeString() {
+        this.nomes = new String[10];
+        this.posicaoLivre = 0;
+    }
+
+    public void adiciona(String nome) {
+        this.nomes[this.posicaoLivre] = nome;
+        this.posicaoLivre++;
+    }
+
+    public int getQuantidadeDeElementos() {
+        return this.posicaoLivre;
+    }
+
+    public String getReferencia(int posicao) {
+        return this.nomes[posicao];
+    }
+}
---
+++ src/module3/arrayslist/generics/TestaGuardador.java
@@ -0,0 +1,24 @@
+package module3.arrayslist.generics;
+
+public class TestaGuardador {
+
+    public static void main(String[] args) {
+
+        GuardadorDeString guardadorDeString = new GuardadorDeString();
+
+        String nome = new String("emanoel");
+        guardadorDeString.adiciona(nome);
+
+        String sobrenome = new String("campos");
+        guardadorDeString.adiciona(sobrenome);
+
+        int tamanho = guardadorDeString.getQuantidadeDeElementos();
+        System.out.println(tamanho);
+
+        String ref = guardadorDeString.getReferencia(0);
+        String ref2 = guardadorDeString.getReferencia(1);
+
+        System.out.println(ref.length());
+        System.out.println(ref2.toUpperCase());
+    }
+}
